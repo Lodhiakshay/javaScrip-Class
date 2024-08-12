2cfcd32894aead7cbc5f71a43a3e30f1bf5f2e09
Lodhiakshay <akshaykumar0416@gmail.com>
2024-08-12 20:49:13 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,27 @@
+// Variables var, let, const
+
+// var -> Global scope, value can be modify everywhere 
+
+// let -> declare only once and modifide it....within scope,  Block scope
+
+// const -> can not changed and redeclare,  Block Scope
+
+// Data Types: string, number, boolean, null, undefined, object
+
+// var name; // declaration
+
+// name = "anuj"  // assignment
+
+// var name = "anuj"  // String
+
+// var name = "Amit"
+
+// let name = "anuj"  // String
+
+//  name = "Amit"
+
+// const name = "anuj"  // String
+
+// console.log(name);
+
+// console.log(name, age, isPass, id, email)  // print 
